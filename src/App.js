4a8bc29874fb99ad6609a76916653d4221be08c1
Nguyen Van Quyet <quyet2k2ocsa@gmail.com>
2023-09-10 21:15:29 +0700

    Code splitting Routes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import { Fragment } from "react";
+import { Fragment, lazy, Suspense } from "react";
+// use lazy, Suspense de su dung ki thuat Code splitting Routes: o trang nao tai trang do
+// Suspense tri hoan , dung toi moi load dl
 import { Route, Routes } from 'react-router-dom'
 import "swiper/scss";
 import Main from "./components/layouts/Main";
-import HomePage from "./pages/HomePage";
-import { Banner } from "./components";
-import MoviePage from "./pages/MoviePage";
-import MovieDetail from "./pages/MovieDetail";
-// import { NavLink } from 'react-router-dom'
-
+// use lazy
+// dynamic import
+const HomePage = lazy(() => import("./pages/HomePage"));
+const MoviePage = lazy(() => import("./pages/MoviePage"));
+const MovieDetail = lazy(() => import("./pages/MovieDetail"));
+const Banner = lazy(() => import("./components/banner/Banner"));
 function App() {
   return (
     <Fragment>
-      <Routes>
-        <Route element={<Main></Main>}>
-          <Route path="/" element={
-            <>
-              <Banner></Banner>
-              <HomePage></HomePage>
-            </>
-          }></Route>
-          <Route path="/movies" element={
-            <MoviePage></MoviePage>
-          }></Route>
-          <Route path="/movies/:movieId" element={
-            <MovieDetail></MovieDetail>
-          }></Route>
-        </Route>
-      </Routes>
-
+      {/* fallback kieu giong loading */}
+      <Suspense fallback={<></>}>
+        <Routes>
+          <Route element={<Main></Main>}>
+            <Route path="/" element={
+              <>
+                <Banner></Banner>
+                <HomePage></HomePage>
+              </>
+            }></Route>
+            <Route path="/movies" element={
+              <MoviePage></MoviePage>
+            }></Route>
+            <Route path="/movies/:movieId" element={
+              <MovieDetail></MovieDetail>
+            }></Route>
+          </Route>
+        </Routes>
+      </Suspense>
     </Fragment>
   );
 }
--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -31,14 +31,83 @@ const MovieDetail = () => {
                     ))}
                 </div>}
             <p className='text-center leading-relaxed max-w-[600px] mx-auto mb-16'>{overview}</p>
-            <MovieCredits></MovieCredits>
+            {/* <MovieCredits></MovieCredits>
             <MovieVideos></MovieVideos>
-            <MovieSimilar></MovieSimilar>
+            <MovieSimilar></MovieSimilar> */}
+            <MovieMeta type="credits"></MovieMeta>
+            <MovieMeta type="videos"></MovieMeta>
+            <MovieMeta type="similar"></MovieMeta>
         </div>
     );
 };
 
-function MovieCredits() {
+function MovieMeta({ type = "videos" }) {
+    const { movieId } = useParams();
+    const { data } = useSWR(tmdbApi.getMovieMeta(movieId, type), fetcher);
+    if (!data) return null;
+    if (type === "credits") {
+        const { cast } = data;
+        if (!cast || cast.length <= 0) return null;
+        return (
+            <>
+                <h2 className='text-center text-2xl mb-10'>Casts</h2>
+                <div className='grid grid-cols-4 gap-10 mb-10'>
+                    {cast.slice(0, 4).map(item => (
+                        <div className='cast-item' key={item.id}>
+                            <img
+                                className='w-full h-[300px] object-cover rounded-md mb-3'
+                                src={tmdbApi.imageOriginal(item.profile_path)}
+                                alt='cast' />
+                            <h3 className='text-lg text-center font-medium'>{item.name}</h3>
+                        </div>
+                    ))}
+                </div>
+            </>
+        )
+    }
+    else {
+        const { results } = data;
+        if (!results || results.length <= 0) return null;
+        if (type === 'videos') {
+            return (
+                <div className='py-10'>
+                    <div className='flex flex-col gap-6'>
+                        {results.slice(0, 1).map(item => (
+                            <div key={item.id}>
+                                <h3 className='mb-3 text-xl font-medium p-3 bg-secondary inline-block'>{item.name}</h3>
+                                <div className='w-full aspect-video'>
+                                    <iframe
+                                        width="560" height="315" src={`https://www.youtube.com/embed/${item.key}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen
+                                        className="w-full h-full object-fill"
+                                    >
+                                    </iframe>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )
+        }
+        if (type === "similar") {
+            return (
+                <div className='py-10'>
+                    <h2 className='text-2xl font-medium mb-10'>Similar movie</h2>
+                    <div className="movies-list">
+                        <Swiper grabCursor={"true"} spaceBetween={40} slidesPerView={"auto"}>
+                            {results && results.length > 0 && results.map(item => (
+                                <SwiperSlide key={item.id}>
+                                    <MovieCard item={item}></MovieCard>
+                                </SwiperSlide>
+                            ))}
+                        </Swiper>
+                    </div>
+                </div>
+            )
+        }
+    }
+    return null;
+}
+{/* function MovieCredits() {
     const { movieId } = useParams();
     const { data } = useSWR(tmdbApi.getMovieMeta(movieId, "credits"), fetcher);
     if (!data) return null;
@@ -108,5 +177,5 @@ function MovieSimilar() {
             </div>
         </div>
     )
-}
+} */}
 export default MovieDetail;
